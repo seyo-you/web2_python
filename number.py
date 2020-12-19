1155d2c23cb2c584bf1801838fa74cd07987e0c0
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-19 12:59:22 -0500

    syntax - data type - number
    python3 number type(https://docs.python.org/3/library/stdtypes.html)

---
+++ number.py
@@ -0,0 +1,22 @@
+#!/Users/seongyongyou/opt/anaconda3/bin/python3
+print(1+1)
+print(2-1)
+print(3*4)
+print(120/9)
+print(120//9)
+print(120%9,"\n")
+a=-10
+b=2.7
+c=25.789
+d=2
+e=3
+print(-a)
+print(+a)
+print(abs(a),"\n")
+print(int(b))
+print(float(b),"\n")
+print(complex(c,1),"\n")
+#print(c.conjugate(c))
+print(divmod(120, 9))
+print(pow(2, 3))
+print(2 ** 3)
