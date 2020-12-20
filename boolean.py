e339bd99676caf3d930a46862993f5325a1d59c7
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-19 23:07:39 -0500

    syntax - data type - boolean
    python3 check contain text in string(https://stackoverflow.com/questions/3437059/does-python-have-a-string-contains-substring-method)
    python3 check exist file in directory

---
+++ boolean.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#Number
+print(1) #Integer
+#String
+print('Hello world')
+#Boolean
+print(True)
+print(False)
+#Expression
+print(1+1)
+print('Hello '+'world') #Hello world
+#Comparison operator
+print(1==1) #True
+print(1<2) #True
+print(1>2) #False
+#Membership operator
+print('world' in 'Hello world') #True
+
+import os.path
+print(os.path.exists('boolean.py')) #True
