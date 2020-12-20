ef590aea9426166d54c6d85e3c73f0870a35e9d7
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-20 12:46:36 -0500

    Conditional statement(if statement)

---
+++ conditional.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_input == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+if user_id == 'syou':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
