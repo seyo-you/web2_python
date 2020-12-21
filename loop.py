77ebd3bb74bf2199db05ca0d7872309f3478dd62
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-20 20:35:55 -0500

    loop
    The Python Language Reference(https://docs.python.org/3/reference/index.html)
    8. Compound statements
    8.2. The while statement
    8.3. The for statement

---
+++ loop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#list
+print(1)
+for value in ['a', 'b', 'c']:
+    print(value)
+print(2)
+print('----range----')
+#range
+for value in range(10):
+    print(value)
+print('----while----')
+#while
+value = 0
+while value<10:
+    print(value)
+    value = value+1
