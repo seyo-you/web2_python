27d8c1246cb7fd2f30c3e466a247e0759d46858c
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-20 22:09:04 -0500

    writing files with CGI
    python3 file write(https://docs.python.org/3/tutorial/inputoutput.html)
    -Redirection

---
+++ process_create.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import cgi
+form = cgi.FieldStorage()
+title = form["title"].value
+description = form["description"].value
+
+opened_file = open('data/'+title, 'w')
+opened_file.write(description)
+
+#Redirection
+print("Location: index.py?id="+title)
+print()
