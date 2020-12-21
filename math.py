615967e8183a100423fa5e81e59b0f9e3b26a530
seyo-you <66897226+seyo-you@users.noreply.github.com>
2020-12-21 01:16:49 -0500

    Module

---
+++ math.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def average(a,b,c):
+    s=a+b+c
+    r=s/3
+    return r
+
+def plus(a,b):
+    r=a+b
+    return r
+
+pi = 3.14
---
+++ module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''import math
+
+print(math.average(1,2,3))
+print(math.plus(1,2))
+print(math.pi)
+'''
+
+from math import average, plus, pi
+
+print(average(1,2,3))
+print(plus(1,2))
+print(pi)
